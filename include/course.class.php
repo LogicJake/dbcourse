85c578d0b88267f2dbffc194200873388e9eaf66
NepoleonAsus <835410808@qq.com>
2018-05-21 18:44:40 +0800

    获取课程列表

--- include/course.class.php
+++ include/course.class.php
@@ -22,3 +22,41 @@ function deletetCourse($cno){
         $res['msg'] = "删除失败：".$db -> error ;
     return $res;
 }
+
+function selectCourseAll($page){
+    global $db,$course;
+    $page_num = 5;  //每页查看5条
+    $start = ($page-1)*$page_num;
+    $end = $start+$page_num;
+
+    $sql = "SELECT COUNT(*) FROM ".$course;
+    if($re = mysqli_query($db,$sql)){
+        $count = mysqli_fetch_row($re)[0];
+        $max_page = $count/$page_num;
+        if($page < $max_page)
+            $res['finished'] = FALSE;
+        else
+            $res['finished'] = TRUE;
+    }
+
+    $sql = "SELECT * FROM ".$course." ORDER BY Cno LIMIT ".$start.",".$end;
+    $res['sql'] = $sql;
+    $data = array();
+    if($re = mysqli_query($db,$sql)){
+        $rows = array();
+        while($row = mysqli_fetch_array($re)) {
+            $rows[] = $row;
+        }
+        foreach($rows as $row){
+            $tmp['no'] = $row[0];
+            $tmp['name'] = $row[1];
+            $tmp['credit'] = $row[2];
+            array_push($data,$tmp);
+        }
+        $res['msg'] = "查询成功";
+        $res['data'] = $data;
+    }
+    else
+        $res['msg'] = "查询失败：".$db -> error ;
+    return $res;
+}
